8e348f2a433d02cbc6358eaff5cd005c5b15719e
Ario <arioabdillah5@gmail.com>
2026-01-01 02:17:16 +0700

    hallo?

---
+++ src/users/user.entity.ts
@@ -0,0 +1,46 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ default: 'user' })
+  role: 'admin' | 'user';
+
+  @Column({ nullable: true })
+  ssn: string;
+
+  @Column({ nullable: true })
+  creditCardNumber: string;
+
+  @Column({ nullable: true })
+  phoneNumber: string;
+
+  @Column({ nullable: true })
+  address: string;
+
+  @Column({ default: 1000 })
+  belance: number;
+
+  @Column({ nullable: true })
+  resetToken: string;
+
+  @Column({ nullable: true })
+  resetTokenExpiry: Date;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
